9878c4262e17d4f81156fab5ea6bd66575b57503
Brooke <brookekullberg@gmail.com>
2020-03-15 20:47:03 -0700

    add console.log to test trailer commits
    Co-authored-by: Brian Pritt <brianpritt@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("test trailer commits for multiple authors");
   return (
     <div className="App">
       <header className="App-header">
